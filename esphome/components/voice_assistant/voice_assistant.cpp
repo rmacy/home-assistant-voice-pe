5c0e9fe5952489af6d039f4c3d260083c2bc74d0
Ryan Macy <ryan@macy.dev>
2026-07-30 22:55:06 -0500

    Fix announcement text lease parsing

--- esphome/components/voice_assistant/voice_assistant.cpp
+++ esphome/components/voice_assistant/voice_assistant.cpp
@@ -1306,9 +1306,11 @@ void VoiceAssistant::timer_tick_() {
 void VoiceAssistant::on_announce(const api::VoiceAssistantAnnounceRequest &msg) {
 #ifdef USE_MEDIA_PLAYER
   if (this->media_player_ != nullptr) {
-    size_t last_non_space = msg.text.find_last_not_of(" \t\r\n");
+    std::string announce_text = msg.text.str();
+    size_t last_non_space = announce_text.find_last_not_of(" \t\r\n");
     this->last_response_requested_answer_ =
-        last_non_space != std::string::npos && (msg.text[last_non_space] == '?' || msg.text[last_non_space] == ';');
+        last_non_space != std::string::npos &&
+        (announce_text[last_non_space] == '?' || announce_text[last_non_space] == ';');
     this->conversation_session_active_ = msg.start_conversation;
     this->followup_speech_started_ = false;
     this->tts_start_trigger_.trigger(msg.text);
